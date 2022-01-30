581a75e755762e9faab6e61796583593bd88bb35
Ed Allard <ed@waywarddistractions.com>
2022-01-29 22:15:28 -0800

    Adding unity project for the virtual prairie - first check-in (simple grid scene, initial shaders and plant model)

---
+++ Unity/VirtualPrairie/Assets/Code/PlantColorManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class StalkColorManager
+{
+	public List<Material> Materials = new List<Material>();
+	public void SetColor(Color color, float GlowIntensity, float Alpha)
+	{
+		Color mainColor = new Color(color.r,color.g,color.b,Alpha);
+		foreach (var mat in Materials)
+		{
+			mat.SetColor("MainColor",mainColor);
+			mat.SetColor("GlowColor",color);
+			mat.SetFloat("GlowIntensity",GlowIntensity);
+		}
+	}
+
+	public void InitFromStalk(Transform stalkT)
+	{
+		var meshes = stalkT.GetComponentsInChildren<MeshRenderer>();
+		foreach (var mesh in meshes)
+		{
+			Materials.Add(mesh.material);
+		}
+	}
+}
+
+public class PlantColorManager : MonoBehaviour
+{
+	[Header("Debug")]
+	public bool DoDebugRainbow = true;
+	public float DebugRainbowCycleTime = 1.0f;
+	public float DebugBrightness = 1.0f;
+	public Color DebugStaticColor = Color.red;
+
+	[Header("Glow")]
+	public float GlowIntensity = 1.0f;
+	public float StalkAlpha = 1.0f;
+	protected List<StalkColorManager> StalkColors = new List<StalkColorManager>();
+
+	float _colorOffset = 0.0f;
+
+
+	public void Awake()
+	{
+		_colorOffset = Random.RandomRange(0,1f);
+		_colorOffset = transform.position.x;
+		foreach (Transform child in transform)
+		{
+			if (child.gameObject.name.Contains("Stalk"))
+			{
+				var s = new StalkColorManager();
+				s.InitFromStalk(child);
+				StalkColors.Add(s);
+			}
+		}
+	}
+
+	float _debugSwirlAlpha = 0;
+
+	public void Start()
+	{
+
+	}
+	
+	public void Update()
+	{
+		_debugSwirlAlpha += Time.deltaTime / DebugRainbowCycleTime;
+		while (_debugSwirlAlpha > 1.0f)
+		{
+			_debugSwirlAlpha -= 1f;
+		}
+
+		DebugStaticColor = Color.HSVToRGB(_debugSwirlAlpha,1f,1.5f,true);
+
+		float offset = 0;
+
+		foreach (var s in StalkColors)
+		{
+			Color c = Color.HSVToRGB(Mathf.Repeat(offset + _colorOffset + _debugSwirlAlpha,1.0f), 1.0f, DebugBrightness,true);
+			s.SetColor(c, GlowIntensity, StalkAlpha);
+			offset += .02f;
+		}
+	}
+
+}
---
+++ Unity/VirtualPrairie/Assets/Code/PrairieWalkCam.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrairieWalkCam : MonoBehaviour
+{
+
+	public float RotSpeed = 5.0f;
+	public float MouseSensitivity = 1.0f;
+	public float WalkSpeed = 1.0f;
+	public float HeadHeight = 1.65f;
+
+	public  void Start()
+	{
+		transform.position = new Vector3(transform.position.x,HeadHeight,transform.position.z);
+	}
+
+	public void LateUpdate()
+	{
+		if (Input.GetMouseButtonDown(1))
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+		}
+
+		if (Input.GetMouseButtonUp(1))
+		{
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+		}
+
+		Vector2 lookDirDelta = getLookDirDeltaFromMouse();
+
+		Quaternion targetRot = Quaternion.Euler(transform.eulerAngles + new Vector3(lookDirDelta.y * RotSpeed * Time.deltaTime, lookDirDelta.x * RotSpeed * Time.deltaTime, 0f));
+		transform.localRotation = targetRot;
+
+		Vector3 walkDirLocal = getWalkInputDir();
+		Vector3 walkDirWorld = Camera.main.transform.TransformDirection(walkDirLocal);
+
+		walkDirWorld.y = 0.0f;
+		walkDirWorld.Normalize();
+		Vector3 walkVel = walkDirWorld * WalkSpeed;
+
+		transform.position += walkVel * Time.deltaTime;
+	}
+
+	public Vector2 getLookDirDeltaFromMouse()
+	{
+		Vector2 retDelta = Vector2.zero;
+		if (Input.GetMouseButton(1))
+		{
+			retDelta.x = Input.GetAxis("Mouse X") * MouseSensitivity;
+			retDelta.y = -Input.GetAxis("Mouse Y") * MouseSensitivity;
+		}
+		return retDelta;
+	}
+
+	public Vector3 getWalkInputDir()
+	{
+		float lr = Input.GetAxis("Horizontal");
+		float fb = Input.GetAxis("Vertical");
+		return new Vector3(lr, 0, fb);
+	}
+}
