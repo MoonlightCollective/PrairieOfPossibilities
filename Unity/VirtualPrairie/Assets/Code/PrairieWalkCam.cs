0a7860fed10803f15e46b2c32b31b767c54c28b7
Ed Allard <ed@waywarddistractions.com>
2022-02-02 16:36:23 -0800

    camera can fly

--- Unity/VirtualPrairie/Assets/Code/PrairieWalkCam.cs
+++ Unity/VirtualPrairie/Assets/Code/PrairieWalkCam.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class PrairieWalkCam : MonoBehaviour
 {
-
 	public float RotSpeed = 5.0f;
 	public float MouseSensitivity = 1.0f;
 	public float KeyLookSensitivity = 1.0f;
 	public float WalkSpeed = 1.0f;
 	public float HeadHeight = 1.65f;
+	public float FloatSpeed = 3.0f;
 
 	public float AccelRate = 1.0f;
 	public float DecelRate = 0.3f;
+	public bool isFlying = true;
 	
 	protected float _desiredSpeed;
 	protected float _curSpeed;
+
 
 	public  void Start()
 	{
@@ -54,16 +56,52 @@ public class PrairieWalkCam : MonoBehaviour
 		else
 			_curSpeed = Mathf.MoveTowards(_curSpeed,_desiredSpeed, Time.deltaTime / DecelRate);
 
-		Vector3 walkVel = walkDirWorld * _curSpeed;
+		float mult = 1;
+		if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+		{
+			mult = 2.5f;
+		}
+
+		Vector3 walkVel = walkDirWorld * _curSpeed * mult;
 		transform.position += walkVel * Time.deltaTime;
+
+		if (isFlying)
+			setHeightFromFlight(mult);
+		else
+			setHeightFromWalk();
+	}
+
+	void setHeightFromFlight(float mult)
+	{
+		float flySpeed = Input.GetAxis("Fly") * FloatSpeed * mult;
+		Vector3 flyVel = flySpeed * Vector3.up;
+		transform.position += flyVel * Time.deltaTime;
 
 		Vector3 castStart = transform.position;
 		RaycastHit hit;
+		float newY = transform.position.y;
+
 		if (Physics.Raycast(castStart, Vector3.down, out hit, 40f, LayerMask.GetMask("Ground"), QueryTriggerInteraction.Ignore))
 		{
-			transform.position = new Vector3(transform.position.x, hit.point.y + HeadHeight, transform.position.z);
+			newY = Mathf.Max(HeadHeight + hit.point.y, transform.position.y);
+// transform.position = new Vector3(transform.position.x, hit.point.y + HeadHeight, transform.position.z);
 		}
-		
+		else
+		{
+			newY = Mathf.Max(HeadHeight, transform.position.y);
+			// transform.position = new Vector3(transform.position.x, HeadHeight, transform.position.z);
+		}
+		transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+	}
+
+	void setHeightFromWalk()
+	{
+		Vector3 castStart = transform.position;
+		RaycastHit hit;
+		if (Physics.Raycast(castStart, Vector3.down, out hit, 40f, LayerMask.GetMask("Ground"), QueryTriggerInteraction.Ignore))
+			transform.position = new Vector3(transform.position.x, hit.point.y + HeadHeight, transform.position.z);
+		else
+			transform.position = new Vector3(transform.position.x, HeadHeight, transform.position.z);
 	}
 
 	public Vector2 getLookDirDeltaFromMouse()
