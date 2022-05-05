0e91905475629904a69409ed9e18f2ac8e5fcee9
Ed Allard <ed@waywarddistractions.com>
2022-05-04 23:25:45 -0700

    Colored noise seems to work.

---
+++ Unity/VirtualPrairie/Assets/Code/PatternEffects/PerlinNoisePattern.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using NaughtyAttributes;
+using UnityEngine;
+
+public class PerlinNoisePattern : PrairiePatternLayer
+{
+	[Space]
+	[Expandable]
+	public PerlinNoiseSettings NoiseSettings;
+	
+	[Space]
+	[Expandable]
+	public ColorizeBrightToGradient ColorizeSettings;
+
+	protected Vector3 _offsetVect = Vector3.zero;
+	protected Vector3 _moveSpeedVect = Vector3.zero;
+	protected Vector3 _noiseScale = Vector3.one;
+
+	public void Start()
+	{
+		_offsetVect = NoiseSettings.InitialOffsets;
+	}
+
+	void updateVects()
+	{
+		_moveSpeedVect = new Vector3(NoiseSettings.MotionSpeedX,NoiseSettings.MotionSpeedY,NoiseSettings.MotionSpeedZ);
+		_noiseScale = NoiseSettings.NoiseScale * 0.001f; // this makes it a better range for the noise generator compared to our field size.
+	}
+	
+	void updateMotion(float deltaTime)
+	{
+		if (_moveSpeedVect.magnitude > 0)
+		{
+			_offsetVect = _offsetVect + (_moveSpeedVect * deltaTime);
+		}
+	}
+
+	public override void Run(float deltaTime, PrairieLayerScene scene, List<StemColorManager> points)
+	{
+		float alpha = BlendSettings.LayerAlpha * scene.SceneSettings.SceneAlpha;
+		Color blendColor;
+		updateVects();
+		updateMotion(deltaTime);
+
+		foreach (var p in points)
+		{
+			Vector3 pos = p.transform.position + _offsetVect;
+			float noiseVal = Mathf.Clamp01((float)NoiseS3D.NoiseCombinedOctaves(pos.x * _noiseScale.x, pos.y * _noiseScale.y, pos.z * _noiseScale.z));
+			float brightness = NoiseSettings.BrightnessCurve.Evaluate(noiseVal);
+			
+			if (ColorizeSettings == null)
+			{
+				blendColor = new Color(brightness,brightness,brightness,alpha);
+			}
+			else
+			{
+				blendColor = ColorizeSettings.ColorForBrightness(brightness,scene);
+				blendColor.a = alpha;
+			}
+
+			p.SetColor(ColorBlend.BlendColors(blendColor,p.CurColor,BlendSettings.BlendMode));
+		}
+	}
+
+}
---
+++ Unity/VirtualPrairie/Assets/Code/PatternEffects/PerlinNoiseSettings.cs
@@ -0,0 +1,28 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+[CreateAssetMenu(fileName = "PerlinNoise Settings", menuName = "Prairie Settings/Perlin Noise Settings", order = 1)]
+public class PerlinNoiseSettings : ScriptableObject
+{
+	[Header("NoiseScale")]
+	public Vector3 NoiseScale = Vector3.one;
+
+	[Header("NosieOffset")]
+	public Vector3 InitialOffsets = Vector3.zero;
+
+	[CurveRange(0f,0f,1f,1f)]
+	public AnimationCurve BrightnessCurve = AnimationCurve.Linear(0f,0f,1f,1f);
+
+	[Header("Offset Motion")]
+	[Range(-10,10)]
+	public float MotionSpeedX = 0f;
+	[Range(-10,10)]
+	public float MotionSpeedY = 0f;
+	[Range(-10,10)]
+	public float MotionSpeedZ = 0f;
+
+	
+}
