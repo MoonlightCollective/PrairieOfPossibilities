c297fc5ed0f24fbb1ac3314429c14520887b2419
Paul McDaniel <paulm@microsoft.com>
2022-05-30 10:46:44 -0700

    - set paul's home ip address to one set of plants in GridPlantTest - added mqtt libs - added mqtt to the scene - turned on dmx send by default in the scene

---
+++ Unity/VirtualPrairie/Assets/Code/Network/MqttController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using M2MqttUnity;
+
+/// <summary>
+/// Examples for the M2MQTT library (https://github.com/eclipse/paho.mqtt.m2mqtt),
+/// </summary>
+public class MqttController : M2MqttUnityClient
+{
+
+}
