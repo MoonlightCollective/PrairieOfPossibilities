0119913ffa0ea198125825b179dbc8a79f85ada6
James Gwertzman <gwertzman@gmail.com>
2022-07-03 01:33:35 -0700

    added debug code

--- Unity/VirtualPrairie/Assets/Code/Plants/WiredPathManager.cs
+++ Unity/VirtualPrairie/Assets/Code/Plants/WiredPathManager.cs
@@ -59,6 +59,7 @@ public class WiredPathManager : MonoBehaviour
 
 	public void NotifyNewLayout()
 	{
+		Debug.Log("New Layout Detected");
 		if (PlantSelectionManager.Instance.IsWiring())
 		{
 			PlantSelectionManager.Instance.NotifyRereshWiring();
