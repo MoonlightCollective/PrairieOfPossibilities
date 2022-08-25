eb3a6195521865e8eb233f6969598fd3e592edc9
Ed Allard <ed@eddyspaghetti.net>
2022-08-24 17:47:16 -0700

    Added booth mqtt trigger wiring to the PortalEventsAndPatterns prefab.  changed common hostname for mqtt broker to 'mqtt' in the CommonStatic.prefab.

--- Unity/VirtualPrairie/Assets/Code/PatternEffects/PatternTriggers/MqttTrigger.cs
+++ Unity/VirtualPrairie/Assets/Code/PatternEffects/PatternTriggers/MqttTrigger.cs
@@ -24,6 +24,7 @@ public class MqttTrigger : MonoBehaviour
 {
 	public List<MessageToTriggerEntry> MessageMappings = new List<MessageToTriggerEntry>();
 	public Color EditGroupColor = new Color(.8f,.8f,.8f);
+	public bool ShowDebug = false;
 
 	public void NotifyMessage(string message,Dictionary<string,object> fields)
 	{
@@ -35,12 +36,18 @@ public class MqttTrigger : MonoBehaviour
 				{
 					if (!PrairieUtil.CheckFieldsAgainstFilter(fields,entry.FieldFilters))
 					{
+						if (ShowDebug)
+							Debug.Log($"{gameObject.name} failed trigger filter check");
 						// skip to next iteration of the outer loop. This message isn't going to get an event fired.
 						continue;
 					}
 				}
+				if (ShowDebug)
+					Debug.Log($"{gameObject.name} emitting trigger");
 				entry.Targets.EmitTrigger(new PrairieTriggerParams(message,entry.SendParams?entry.Param0:0f,entry.SendParams?entry.Param1:0f));
 			}
 		}
+		if (ShowDebug)
+			Debug.Log($"{gameObject.name} no message mapping for {message}");
 	}
 }
