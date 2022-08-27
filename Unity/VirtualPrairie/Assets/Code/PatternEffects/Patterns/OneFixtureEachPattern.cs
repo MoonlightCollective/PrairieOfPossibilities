0d087d51612c464a23b3f556b17ed502395a8f8b
Paul McDaniel <paul_mcdaniel@hotmail.com>
2022-08-27 00:41:24 -0700

    - added a one fixture each pattern

---
+++ Unity/VirtualPrairie/Assets/Code/PatternEffects/Patterns/OneFixtureEachPattern.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using NaughtyAttributes;
+
+public class OneFixtureEachPattern : PrairiePatternMonochromaticBase
+{
+	[Expandable]
+	public OnePointEachSettings OnePointEachSettings;
+
+	float _delayTimer = -1f;
+	int _index = -1;
+
+    public override void Run(float deltaTime, PrairieLayerGroup group, List<StemColorManager> points) 
+    {
+		_delayTimer -= deltaTime;
+		if (_delayTimer <= 0 )
+		{
+            // move to the next fixture
+    		_index++;
+	        Debug.Log($"Lighting up Fixture {_index}");
+			_delayTimer += OnePointEachSettings.DelayTimeSec;
+			if (_index * PrairieDmxController.PointsPerFixture >= PrairieUtil.Points.Count)
+			{
+				_index = 0;
+			}
+		}
+		Color blendColor = OnePointEachSettings.PointColor.Color(group) * BlendSettings.LayerAlpha * group.GroupAlpha;
+        // light up every point in this fixture
+        for (var pointIndex = _index * PrairieDmxController.PointsPerFixture; 
+            pointIndex < (_index * PrairieDmxController.PointsPerFixture) + PrairieDmxController.PointsPerFixture; 
+            ++pointIndex) 
+        {
+            points[pointIndex].SetColor(ColorBlend.BlendColors(blendColor,points[pointIndex].CurColor,BlendSettings.BlendMode));
+        }
+    }
+}
