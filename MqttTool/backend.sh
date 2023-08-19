4a3fab0e910903d4fc49b006ecb834920cdd9f77
James Gwertzman <gwertzman@gmail.com>
2023-08-18 18:22:11 -0700

    mqtt sh files

---
+++ MqttTool/backend.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+cd /Users/moonlight/GitHub/PraireOfPossibilites/MqttTool/
+npm run dev
+
+#on run {input, parameters}
+#	tell application "Terminal" to activate
+#	tell application "System Events" to keystroke "t" using {command down}
+#	tell application "Terminal" to do script "/Users/moonlight/GitHub/PraireOfPossibilites/MqttTool/backend.sh" in front window#
+#	return input
+#end run
---
+++ MqttTool/mqtt/frontend.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+cd /Users/moonlight/GitHub/PraireOfPossibilites/MqttTool/mqtt
+npm start
+
+#on run {input, parameters}
+#	tell application "Terminal" to activate
+#	tell application "System Events" to keystroke "t" using {command down}
+#	tell application "Terminal" to do script "/Users/moonlight/GitHub/PraireOfPossibilites/MqttTool/mqtt/frontend.sh" in front window#
+#	return input
+#end run
