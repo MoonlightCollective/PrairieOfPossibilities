f05d0fe72ce499c8c3cdd0bfe5b7be2434ddadd4
James Gwertzman <gwertzman@gmail.com>
2023-08-18 17:59:22 -0700

    added apple script comments

--- AudioPipeline/stage1.sh
+++ AudioPipeline/stage1.sh
@@ -3,3 +3,12 @@ cd /Users/moonlight/Documents/ContainerData/
 conda run --live-stream -n pyannote python ./stage1.py
 #conda activate pyannote
 #python stage1.py
+
+
+
+#on run {input, parameters}
+#	tell application "Terminal" to activate
+#	tell application "System Events" to keystroke "t" using {command down}
+#	tell application "Terminal" to do script "/Users/moonlight/Documents/ContainerData/stage1.sh" in front window#
+#	return input
+#end run
